848a1cec2cfe946443b8f0f911f2581ff8f40bbb
harshraj2717 <harshraj2717@gmail.com>
2024-11-10 17:23:54 +0530

    add AI prompting apis

---
+++ ai/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from prompter import prompter
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class PromptModel(BaseModel):
+    project_description: str
+
+
+@app.post("/")
+async def main(request: PromptModel):
+    return {"data": prompter(request.project_description)}
